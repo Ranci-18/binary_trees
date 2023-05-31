eb944e3d379e73c6de0d2b96997b138b2baff965
RIvAL-411 <cliffomanyo@gmail.com>
2023-05-31 02:30:25 -0700

    task 15 fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,10 +8,10 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (1);
-	if ((!tree->left && tree->right) || (tree->left && !tree->right))
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-	return (1);
+	return (0);
 }
