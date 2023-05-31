632c05e23d40cdae3ae6eceb4520ecc58fa95070
RIvAL-411 <cliffomanyo@gmail.com>
2023-05-31 12:30:08 -0700

    task 16 fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,7 +8,7 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0);
+		return (1);
 	if (!tree->left && !tree->right)
 		return (1);
 	if (tree->left && tree->right)
